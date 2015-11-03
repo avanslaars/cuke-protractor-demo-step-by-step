e0c2fdacf46a93687542387de4d0684c02e02217
Andy Van Slaars <avanslaars@urbn.com>
2015-11-03 16:03:36 -0500

    Replaced comments in steps with TODOs

--- features/step_definitions/say-hello.js
+++ features/step_definitions/say-hello.js
@@ -1,22 +1,22 @@
 
 module.exports = function(){
   this.Given(/^I am on the home page with an empty input$/, function (done) {
-    // Write code here that turns the phrase above into concrete actions
+    //TODO: Navigate to the app's home Page
     done();
   });
 
   this.Given(/^I have typed my name into the input$/, function (done) {
-    // Write code here that turns the phrase above into concrete actions
+    //TODO: Type some input into the input field
     done();
   });
 
   this.When(/^I click on the button$/, function (done) {
-    // Write code here that turns the phrase above into concrete actions
+    //TODO: Click the button
     done.fail();
   });
 
   this.Then(/^The message should include my name$/, function (done) {
-    // Write code here that turns the phrase above into concrete actions
+    //TODO: Make assertions about the content of the page
     done.pending();
   });
 }
