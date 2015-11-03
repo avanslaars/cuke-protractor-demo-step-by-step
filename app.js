1d75c146ae54722bbfc1cfc78a5db28f9523f4ab
Andy Van Slaars <avanslaars@urbn.com>
2015-11-03 15:33:11 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  angular.module('SampleApp',[])
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['$scope'];
+
+  function MainController($scope){
+    $scope.SampleMessage = "Welcome to my pointless app"
+
+    $scope.handleSubmit = function(){
+      if($scope.suppliedName){
+        $scope.SampleMessage = "Hello " + $scope.suppliedName
+      }
+      else {
+        $scope.SampleMessage = "Please supply a name"
+      }
+      $scope.suppliedName = ""
+    }
+  }
+
+}());
---
+++ protractor-conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  baseUrl: 'http://localhost:8080',
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  framework: 'cucumber',
+  specs: [
+    '../features/*.feature'
+  ],
+  capabilities: {
+    browserName: 'chrome',
+    version: '',
+    platform: 'ANY'
+  },
+  cucumberOpts: {
+    require: '../features/step_definitions/*.js',
+    format: 'progress'
+  }
+}
