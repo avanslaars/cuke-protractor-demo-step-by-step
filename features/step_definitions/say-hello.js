ebe3106e509bc340440275363cadf9cee1935690
Andy Van Slaars <avanslaars@urbn.com>
2015-11-03 15:55:42 -0500

    Pasted in step def shells

--- features/step_definitions/say-hello.js
+++ features/step_definitions/say-hello.js
@@ -1,4 +1,22 @@
 
 module.exports = function(){
-  
+  this.Given(/^I am on the home page with an empty input$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^I have typed my name into the input$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^I click on the button$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^The message should include my name$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 }
